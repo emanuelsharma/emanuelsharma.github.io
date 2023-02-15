cdf54f336ec4b1a4a1e220860266fc9d709cd507
Emanuel Sharma <send.emanuel@hotmail.com>
2023-02-14 21:59:18 -0500

    Updated styling

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 import Hero from "./hero/hero";
 import FontStyles from "./font";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
+
 const App = () => (
     <>
+        <GlobalStyle/>
         <FontStyles/>
         <Hero/>
     </>
--- src/hero/hero.js
+++ src/hero/hero.js
@@ -2,41 +2,40 @@ import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { EffectComposer, SSAO, HueSaturation } from "react-postprocessing"
 
 const portraitPath = require('./portrait.glb');
 
 const Title = styled.h1`
-  position: absolute;
-  top: 0;
-  left: 0;
+  flex: 1;
+  margin: 0;
   padding: 0.05em;
   font-family: 'Arges', sans-serif;
   font-size: 30em;
   line-height: 0.8em;
   text-transform: uppercase;
   text-align: left;
-  color: black;
-  margin: 0;
+  color: #DCD8CD;
+  background-color: #46736C;
 `;
 
 const HeroContainer = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
 `;
 
 const PortraitContainer = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 40em;
-  height: 100%;
+  flex: 1;
+  background-color: #6BBFB3
 `;
-const Divider = styled.hr`
-  border: 10px solid black;
-  width: 20%;
-  margin-top: 0.1em;
+const Divider = styled.div`
+  float: left;
+  width: 40%;
+  height: 32px;
+  margin: 0.1em;
   margin-left: 0.01em;
+  background-color: #DCD8CD;
 `;
 
 const Portrait = () => {

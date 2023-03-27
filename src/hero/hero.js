9a2428ea58054e4e3a67927981b17e788b902b50
Emanuel Sharma <send.emanuel@hotmail.com>
2023-03-27 00:31:24 -0400

    Animated background

--- src/hero/hero.js
+++ src/hero/hero.js
@@ -5,38 +5,119 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const portraitPath = require('./portrait.glb');
 
-const Title = styled.h1`
-  flex: 1;
-  margin: 0;
-  padding: 0.05em;
-  font-family: 'Arges', sans-serif;
-  font-size: 30em;
-  line-height: 0.8em;
-  text-transform: uppercase;
-  text-align: left;
-  color: #DCD8CD;
-  background-color: #46736C;
+const Title = styled.div`
+    position: absolute;
+    z-index: 2;
+    left: 0;
+    top: 0;
+    padding: 1rem;
+    font-family: 'Arges', sans-serif;
+    font-size: 15rem;
+    line-height: 0.8em;
+    text-transform: uppercase;
+    color: #fde8c6;
+`;
+
+const Button = styled.button`
+position: relative;
+    width: 150px;
+    margin-right: 1rem;
+    background-color: #fde8c6;
+    border: 3px solid #fde8c6;
+    border-radius: 0.5em;
+    //box-shadow: #422800 4px 4px 0 0;
+    color: #7abdae;
+    cursor: pointer;
+    display: inline-block;
+    font-weight: 600;
+    font-size: 1.2rem;
+    padding: 0 1em;
+    line-height: 3em;
+    text-align: center;
+    text-decoration: none;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    transition: all 0.15s;
+    &:hover {
+      background-color: transparent;
+      color: #fde8c6;
+      border-color: #fde8c6;
+    }
+`;
+
+const Body = styled.div`
+    font-size: 5rem;
+    line-height: 0.8em;
+    text-transform: uppercase;
+    color: #fde8c6;
 `;
 
 const HeroContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  align-items: stretch;
+    width: 100%;
+    height: 100vh;
+    background-color: #c5edd0;
 `;
 
 const PortraitContainer = styled.div`
-  flex: 1;
-  background-color: #6BBFB3
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 40%;
+    height: 100%;
+    z-index: 2;
 `;
 const Divider = styled.div`
-  float: left;
-  width: 40%;
-  height: 32px;
-  margin: 0.1em;
-  margin-left: 0.01em;
-  background-color: #DCD8CD;
+    float: left;
+    width: 40%;
+    height: 32px;
+    margin: 0.1em;
+    margin-left: 0.01em;
+    background-color: #fde8c6;
 `;
+
+const AnimatedBackground = () => {
+    return (
+        <svg
+        xmlns="http://www.w3.org/2000/svg" 
+        xmlnsXlink="http://www.w3.org/1999/xlink" 
+        style={{width: "80%", height: "100%", margin: 'auto', display: 'block', zIndex: '1', position: 'absolute'}} 
+        preserveAspectRatio="xMidYMid"
+        viewBox="0 0 1920 1080">
+            <g transform="translate(960,540) scale(1,1) translate(-960,-540)">
+                <g transform="translate(-250, 0) scale(120)" opacity="1">
+                    <path 
+                    d="M13.392000000000001 0 C13.392000000000001 2.88 15.131713485299045 4.09049031276437 13.882128316640477 6.685280652323337 S10.601450096000088 8.674636583858703 8.349775426492162 10.470287213211856 S6.236394937482546 14.380828981103356 3.4286025503988937 15.021689270897541 S-0.17220796045931008 13.697094889733169 -2.980000347542962 13.056234599938984 S-7.355056197531441 13.84211011122056 -9.606730867039367 12.046459481867407 S-10.816189910290628 8.405361373821307 -12.065775078949196 5.81057103426234 S-15.408000000000003 2.8800000000000017 -15.408000000000003 1.886935788126242e-15 S-13.315360247607765 -3.2157806947033705 -12.065775078949198 -5.810571034262337 S-11.858405536547297 -10.25080885251425 -9.60673086703937 -12.046459481867403 S-5.787792734626617 -12.415374310144799 -2.980000347542965 -13.056234599938984 S0.6208101633152383 -15.662549560691726 3.42860255039889 -15.021689270897541 S6.098100756984232 -12.265937842565009 8.349775426492158 -10.470287213211858 S12.632543147981908 -9.280070991882306 13.882128316640475 -6.68528065232334 S13.392000000000001 -2.880000000000003 13.392000000000001 -3.280093986836271e-15"
+                    fill="#7abdae" 
+                    strokeWidth="0">
+                        <animateTransform 
+                        attributeName="transform" 
+                        type="rotate"
+                        dur="40s" 
+                        repeatCount="indefinite" 
+                        values="0;51.42857142857143"
+                        />
+                    </path>
+                </g>
+                {/*
+                <animateTransform 
+                attributeName="transform" 
+                type="scale" 
+                dur="3s" 
+                repeatCount="indefinite" 
+                values="1 1; 1.1 1; 1 1"
+                keySplines="
+                    0.1 0.8 0.2 1;
+                    0.1 0.8 0.2 1;
+                    0.1 0.8 0.2 1"
+                keyTimes="0;0.5;1"
+                calcMode="spline"
+                />
+                */}
+            </g>
+        </svg>
+    );
+}
 
 const Portrait = () => {
     const ref = useRef();
@@ -49,9 +130,6 @@ const Portrait = () => {
         ref.current.rotation.z = (1 + Math.sin(t / 1.5)) / 20 + Math.PI / 64
         ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10 - 2
     });
-
-    console.log(nodes);
-    console.log(materials);
     
     const scale = 1.5;
 
@@ -79,12 +157,18 @@ const Portrait = () => {
 
 const Hero = () => (
     <HeroContainer>
+        <AnimatedBackground/>
         <Title>
             Emanuel
-            <br/>
             Sharma
-            <Divider/>
+            <Body>
+                Software Developer
+            </Body>
         </Title>
+        <div style={{zIndex: '1', padding: '1rem', position: 'absolute', bottom: '0', left: '0', display: 'flex', height: '64px'}}>
+            <Button>See Details</Button>
+            <Button>Contact</Button>
+        </div>
         <PortraitContainer>
             <Canvas camera={{near: 0.00001}}>
                 <Portrait/>
